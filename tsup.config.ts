d4939f288461545eaca1aba2680218c941caa495
mengpeng <pengmeng@yangqianguan.com>
2025-03-04 20:59:55 +0800

    chore: 调整下模板

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,18 @@
 import { defineConfig } from 'tsup'
-
+const env = process.env.NODE_ENV;
 export default defineConfig({
   entry: [
     'src/index.ts',
   ],
+  sourcemap: env === 'development',
+  minify: env === 'production',
+  watch: env === 'development',
   format: ['cjs'],
   shims: false,
   dts: false,
+  target: 'es2022', 
   external: [
     'vscode',
   ],
+  noExternal:[]
 })
